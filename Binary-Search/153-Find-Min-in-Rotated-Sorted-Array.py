3364ce6004c9f571e7d593845e66b3a73ef466e6
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-11-20 19:21:23 -0400

    O(LOG N) - Binary Search custom

---
+++ Binary-Search/153-Find-Min-in-Rotated-Sorted-Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums)-1
+        mid = 0
+        while low < high:
+            mid = (low+high)//2
+            if nums[mid]>nums[high]:
+                low = mid+1
+            else:
+                high = mid
+        return nums[low]
