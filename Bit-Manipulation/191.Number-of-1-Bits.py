d2a02291e2842a8bb910d0df7f90ff5d4e57f768
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-12-09 14:07:35 -0400

    3 approaches

--- Bit-Manipulation/191.Number-of-1-Bits.py
+++ Bit-Manipulation/191.Number-of-1-Bits.py
@@ -2,3 +2,19 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         num = str(bin(n)[2:])
         return num.count('1')
+        ## solution 2 - right shift technique
+        res = 0
+        while n>0:
+            res += n%2 # remainder
+            n = n >> 1 # right shift
+        return res
+        # solution 3 - using AND operation
+        res = 0
+        while n:
+            n = n & (n-1)
+            res += 1
+        return res
+
+
+
+
