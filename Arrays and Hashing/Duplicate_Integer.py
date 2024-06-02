5a5c5f878f7fedfc369033f83ab036de94965489
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2024-06-02 01:23:36 -0300

    using hashmao

---
+++ Arrays and Hashing/Duplicate_Integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashMap = {}
+        for i, ele in enumerate(nums):
+            if ele in hashMap:
+                return True
+            hashMap[ele] = i
+        return False
